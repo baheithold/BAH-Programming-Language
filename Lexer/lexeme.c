aa54c4c6e49507cb9b61a9ad01b2b9314a233f90
baheithold <baheithold@crimson.ua.edu>
2020-10-21 22:42:15 -0500

    Create Lexeme constructor

--- Lexer/lexeme.c
+++ Lexer/lexeme.c
@@ -10,6 +10,10 @@
 #include "types.h"
 
 
+#include <assert.h>
+#include <stdlib.h>
+
+
 struct Lexeme {
     char    *type;
     char    *stringValue;
@@ -17,3 +21,24 @@ struct Lexeme {
     double   realValue;
     int      lineNumber;
 };
+
+
+/********** Public Function Definitions **********/
+
+Lexeme *newLexeme(char *type, char *value) {
+    assert(type != NULL);
+    Lexeme *result = malloc(sizeof(Lexeme));
+    assert(result != NULL);
+    result->type = type;
+    if (type == INTEGER_TYPE) {
+        result->integerValue = atoi(value);
+    }
+    else if (type == REAL_TYPE) {
+        result->realValue = atof(value);
+    }
+    else if (type == STRING_TYPE) {
+        result->stringValue = value;
+    }
+    result->lineNumber = 0;
+    return result;
+}
