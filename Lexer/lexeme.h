a3105a79ee287799e4e6b757a015dd5a2c5425a8
baheithold <baheithold@crimson.ua.edu>
2020-10-21 22:26:41 -0500

    Created lexeme header

---
+++ Lexer/lexeme.h
@@ -0,0 +1,26 @@
+/*
+ *  Author:         Brett Heithold
+ *  File:           lexeme.h
+ *  Created:        10/21/2020
+ *  Last Revised:   10/21/2020
+ */
+
+#ifndef __LEXEME_H__
+#define __LEXEME_H__
+
+
+#include <stdio.h>
+
+
+typedef struct Lexeme Lexeme;
+
+extern Lexeme   *newLexeme(char *, char *);
+extern char     *getLexemeType(Lexeme *);
+extern int       getLexemeIntegerValue(Lexeme *);
+extern double    getLexemeRealValue(Lexeme *);
+extern char     *getLexemeStringValue(Lexeme *);
+extern int       getLexemeLineNumber(Lexeme *);
+extern void      setLexemeLineNumber(Lexeme *, int);
+extern void      printLexeme(FILE *, Lexeme *);
+
+#endif // !__LEXEME_H__
