7893b8b1752874f99208670f8d17562ffcc200ab
baheithold <baheithold@crimson.ua.edu>
2020-10-21 23:05:08 -0500

    Implemented more basic Lexeme functions

--- Lexer/lexeme.c
+++ Lexer/lexeme.c
@@ -42,3 +42,68 @@ Lexeme *newLexeme(char *type, char *value) {
     result->lineNumber = 0;
     return result;
 }
+
+
+char *getLexemeType(Lexeme *lexeme) {
+    assert(lexeme != NULL);
+    return lexeme->type;
+}
+
+
+int getLexemeIntegerValue(Lexeme *lexeme) {
+    assert(lexeme != NULL);
+    return lexeme->integerValue;
+}
+
+
+double getLexemeRealValue(Lexeme *lexeme) {
+    assert(lexeme != NULL);
+    return lexeme->realValue;
+}
+
+
+char *getLexemeStringValue(Lexeme *lexeme) {
+    assert(lexeme != NULL);
+    return lexeme->stringValue;
+}
+
+
+int getLexemeLineNumber(Lexeme *lexeme) {
+    assert(lexeme != NULL);
+    return lexeme->lineNumber;
+}
+
+
+void setLexemeLineNumber(Lexeme *lexeme, int line) {
+    assert(lexeme != NULL);
+    lexeme->lineNumber = line;
+}
+
+
+void printLexeme(FILE *fp, Lexeme *lexeme) {
+    assert(lexeme != NULL);
+
+    if (lexeme->type == INTEGER_TYPE) {
+        // Print INTEGER_TYPE Lexeme
+        fprintf(fp, "%s %d", lexeme->type, lexeme->integerValue);
+    }
+    else if (lexeme->type == REAL_TYPE) {
+        // Print REAL_TYPE Lexeme
+        fprintf(fp, "%s %f", lexeme->type, lexeme->realValue);
+    }
+    else if (lexeme->type == STRING_TYPE) {
+        // Print REAL_TYPE Lexeme
+        fprintf(fp, "%s %s", lexeme->type, lexeme->stringValue);
+    }
+    else {
+        fprintf(fp, "%s", lexeme->type);
+        if (lexeme->type == STRING_TYPE) {
+            fprintf(fp, " \"%s\"", lexeme->stringValue);
+        }
+        else {
+            if (lexeme->stringValue != NULL) {
+                fprintf(fp, " %s", lexeme->stringValue);
+            }
+        }
+    }
+}
