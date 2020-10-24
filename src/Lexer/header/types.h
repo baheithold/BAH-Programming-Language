08d457ee53eb00e0615fb3b9a3d653018786cf83
baheithold <baheithold@crimson.ua.edu>
2020-10-24 02:05:50 -0500

    Updated last revision date

--- src/Lexer/header/types.h
+++ src/Lexer/header/types.h
@@ -2,7 +2,7 @@
  *  Author:         Brett Heithold
  *  File:           types.h
  *  Created:        10/21/2020
- *  Last Revised:   10/21/2020
+ *  Last Revised:   10/24/2020
  */
 
 #ifndef __TYPES_H__
