26010fff82695f2d2aeddf99fbfea82ff6efeb78
baheithold <baheithold@crimson.ua.edu>
2020-10-23 20:10:31 -0500

    Removed an unused line that was commented out

--- Lexer/src/scanner.c
+++ Lexer/src/scanner.c
@@ -31,7 +31,6 @@ int main(int argc, char **argv) {
     // Lexically analyze the provided file
     Lexeme *lexeme = lex(fp);
     while (getLexemeType(lexeme) != END_OF_INPUT) {
-        //char *type = getLexemeType(lexeme);
         printLexeme(stdout, lexeme);
         fprintf(stdout, "\n");
         lexeme = lex(fp);
