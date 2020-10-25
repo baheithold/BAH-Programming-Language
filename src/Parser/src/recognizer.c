c5a42bf120ee2e8d995c9332029b5d52cdb917b7
baheithold <baheithold@crimson.ua.edu>
2020-10-25 15:58:14 -0500

    Starting on recognizer

---
+++ src/Parser/src/recognizer.c
@@ -0,0 +1,16 @@
+/*
+ *  Author:         Brett Heithold
+ *  File:           recognizer.c
+ *  Created on:     10/25/2020
+ *  Last Revision:  10/25/2020
+ */
+
+
+#include "../../Lexer/header/lexeme.h"
+#include "../../Lexer/header/types.h"
+#include "../../Lexer/header/lexer.h"
+
+
+int main(int argc, char **argv) {
+    return 0;
+}
