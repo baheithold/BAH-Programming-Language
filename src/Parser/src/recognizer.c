80b8694cd533be01e7a1a4999ff3c4a88f2f66e5
baheithold <baheithold@crimson.ua.edu>
2020-10-25 18:51:41 -0500

    Recognizer now checks for proper source file

--- src/Parser/src/recognizer.c
+++ src/Parser/src/recognizer.c
@@ -10,7 +10,50 @@
 #include "../../Lexer/header/types.h"
 #include "../../Lexer/header/lexer.h"
 
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+
+/********** Helper Function Prototypes **********/
+
+bool checkFileExtension(char *);
+
 
 int main(int argc, char **argv) {
+
+    // check number of command line arguments
+    if (argc != 2) {
+        fprintf(stdout, "Usage: recognizer <filename.bah>\n");
+        return 0;
+    }
+
+    // Open and error check file
+    FILE *fp;
+    if (checkFileExtension(argv[1])) {
+        fp = fopen(argv[1], "r");
+        if (!fp) {
+            fprintf(stdout, "ERROR: Failed to open file <%s>\n", argv[1]);
+            return 0;
+        }
+    }
+
     return 0;
+}
+
+
+bool checkFileExtension(char *filename) {
+    assert(filename != NULL);
+    int length = strlen(filename);
+    if (length < 5) {
+        fprintf(stdout, "ERROR: Invalid file name\n");
+        return 1;
+    }
+    else if (filename[length - 4] != '.' || filename[length - 3] != 'b'
+            || filename[length - 2] != 'a' || filename[length - 1] != 'h') {
+        fprintf(stdout, "ERROR: Unrecognized file type\n");
+        return false;
+    }
+    return true;
 }
