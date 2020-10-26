46d31a849c014cfab61471a17e72af37f072e7f2
baheithold <baheithold@crimson.ua.edu>
2020-10-25 21:32:32 -0500

    Working on check, advance, match, and matchNoAdvance function implementations

--- src/Parser/src/parser.c
+++ src/Parser/src/parser.c
@@ -9,6 +9,8 @@
 #include "../header/parser.h"
 #include "../../Lexer/header/lexeme.h"
 
+#include <assert.h>
+
 
 /********** Global Variables **********/
 static Lexeme *currentLexeme;
@@ -18,4 +20,25 @@ static Lexeme *currentLexeme;
 
 void check(char *type) {
     return getLexemeType(currentLexeme) == type;
+}
+
+void advance(void) {
+    currentLexeme = lex();
+}
+
+Lexeme *match(char *type) {
+    assert(type != NULL);
+    matchNoAdvance();
+    advance();
+    return currentLexeme;
+}
+
+void matchNoAdvance(char *type) {
+    assert(type != NULL);
+    assert(currentLexeme != NULL);
+    if (!check(type)) {
+        fprintf(stdout, "ERROR, line %d: ", getLexemeLineNumber(currentLexeme));
+        fprintf(stdout, "expected %s, but encountered %s\n", type, getLexemeType(currentLexeme));
+        exit(1);
+    }
 }
