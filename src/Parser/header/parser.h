c1bf2f211effb744a4c62b6091019c8cb6bd5f4f
baheithold <baheithold@crimson.ua.edu>
2020-10-26 02:23:33 -0500

    Created function prototypes for parser

--- src/Parser/header/parser.h
+++ src/Parser/header/parser.h
@@ -9,13 +9,78 @@
 #define __PARSER_H__
 
 
-#include "Lexeme.h"
+#include "../../Lexer/header/Lexeme.h"
+
+#include <stdbool.h>
 
 
+/********** Lexer Interface Function Prototypes **********/
 extern void      advance(void);
 extern Lexeme   *match(void);
 extern void      matchNoAdvance(char *);
 extern int       check(char *);
+
+
+/********** Grammar Function Prototypes **********/
+extern void program(void);
+extern void import(void);
+extern void statement(void);
+extern void statementList(void);
+extern void optStatementList(void);
+extern void definition(void);
+extern void variableDefinition(void);
+extern void functionDefinition(void);
+extern void classDefinition(void);
+extern void optInheritance(void);
+extern void functionCall(void);
+extern void parameterList(void);
+extern void optParameterList(void);
+extern void expression(void);
+extern void optExpression(void);
+extern void expressionList(void);
+extern void optExpressionList(void);
+extern void unary(void);
+extern void lambdaDef(void);
+extern void block(void);
+extern void loop(void);
+extern void forLoop(void);
+extern void whileLoop(void);
+extern void ifStatement(void);
+extern void returnStatement(void);
+extern void elseStatement(void);
+extern void optElseStatement(void);
+extern void unaryOperator(void);
+extern void binaryOperator(void);
+extern void logicalOperator(void);
+extern void comparator(void);
+
+
+/********** Predicate Function Prototypes **********/
+extern bool programPending(void);
+extern bool importPending(void);
+extern bool statementPending(void);
+extern bool statementListPending(void);
+extern bool definitionPending(void);
+extern bool variableDefinitionPending(void);
+extern bool functionDefinitionPending(void);
+extern bool classDefinitionPending(void);
+extern bool functionCallPending(void);
+extern bool parameterListPending(void);
+extern bool expressionPending(void);
+extern bool expressionListPending(void);
+extern bool unaryPending(void);
+extern bool lambdaDefinitionPending(void);
+extern bool blockPending(void);
+extern bool loopPending(void);
+extern bool forLoopPending(void);
+extern bool whileLoopPending(void);
+extern bool ifStatementPending(void);
+extern bool returnStatementPending(void);
+extern bool elseStatementPending(void);
+extern bool unaryOperatorPending(void);
+extern bool binaryOperatorPending(void);
+extern bool logicalOperatorPending(void);
+extern bool comparatorPending(void);
 
 
 #endif // !__PARSER_H__
