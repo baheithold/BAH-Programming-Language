cef0c7ba50cbfe9473a0ec4152d7f2702d6f1505
baheithold <baheithold@crimson.ua.edu>
2020-10-25 20:31:45 -0500

    Created prototypes for advance, check, match, and matchNoAdvance functions

---
+++ src/Parser/header/parser.h
@@ -0,0 +1,18 @@
+/*
+ *  Author:         Brett Heithold
+ *  File:           parser.h
+ *  Created on:     10/25/2020
+ *  Last revision:  10/25/2020
+ */
+
+#ifndef __PARSER_H__
+#define __PARSER_H__
+
+
+extern void      advance(void);
+extern Lexeme   *match(void);
+extern void      matchNoAdvance(char *);
+extern int       check(char *);
+
+
+#endif // !__PARSER_H__
