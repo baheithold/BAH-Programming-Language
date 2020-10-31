6e137df2a2e1690287035a8caf7c0ea442cab8a7
baheithold <baheithold@crimson.ua.edu>
2020-10-31 13:20:26 -0500

    Working on setting up testing files

---
+++ src/environment/testing/test-env.c
@@ -0,0 +1,16 @@
+/*
+ *  Author:         Brett Heithold
+ *  File:           test-env.c
+ *  Created on:     10/31/2020
+ *  Last revision:  10/31/2020
+ */
+
+
+#include "../header/environment.h"
+#include "../../lexer/header/lexeme.h"
+#include "../../lexer/header/types.h"
+
+
+int main(void) {
+    return 0;
+}
