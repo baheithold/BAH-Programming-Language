db0023358a51712847165b07ed8c802758eb5f4b
baheithold <baheithold@crimson.ua.edu>
2020-10-31 15:24:15 -0500

    Added left/right child pointers and their appropriate getters/setters

--- src/lexer/header/lexeme.h
+++ src/lexer/header/lexeme.h
@@ -2,7 +2,7 @@
  *  Author:         Brett Heithold
  *  File:           lexeme.h
  *  Created:        10/21/2020
- *  Last Revised:   10/24/2020
+ *  Last Revised:   10/31/2020
  */
 
 #ifndef __LEXEME_H__
@@ -23,6 +23,10 @@ extern char     *getLexemeStringValue(Lexeme *);
 extern void      setLexemeValue(Lexeme *, char *);
 extern int       getLexemeLineNumber(Lexeme *);
 extern void      setLexemeLineNumber(Lexeme *, int);
+extern Lexeme   *getLexemeLeftChild(Lexeme *);
+extern void      setLexemeLeftChild(Lexeme *, Lexeme *);
+extern Lexeme   *getLexemeRightChild(Lexeme *);
+extern void      setLexemeRightChild(Lexeme *, Lexeme *);
 extern void      printLexeme(FILE *, Lexeme *);
 
 #endif // !__LEXEME_H__
