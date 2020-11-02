3213004cde437966807f4f545c910b57e838f5c9
baheithold <baheithold@crimson.ua.edu>
2020-11-01 18:49:16 -0600

    Implemented environment functions

--- src/environment/src/environment.c
+++ src/environment/src/environment.c
@@ -14,14 +14,72 @@
 Lexeme *cons(char *, Lexeme *, Lexeme *);
 Lexeme *car(Lexeme *);
 Lexeme *cdr(Lexeme *);
-void    setCar(Lexeme *, Lexeme *);
-void    setCdr(Lexeme *, Lexeme *);
+Lexeme *cadr(Lexeme *);
+void    set_car(Lexeme *, Lexeme *);
+void    set_cdr(Lexeme *, Lexeme *);
+bool    sameVariable(Lexeme *, Lexeme *);
 
 
 /********** Public Function Definitions **********/
 
 Lexeme *createEnvironment(void) {
-    return cons(ENVIRONMENT, NULL, cons(VALUES, NULL, NULL));
+    return extend(NULL, NULL, NULL);
+}
+
+Lexeme *lookupEnvironment(Lexeme *variable, Lexeme *environment) {
+    assert(variable != NULL);
+    assert(environment != NULL);
+
+    while (environment != NULL) {
+        Lexeme *vars = car(environment);
+        Lexeme *vals = cadr(environment);
+        while (vars != NULL) {
+            if (isSameVariable(variable, car(vars))) {
+                return car(vals);
+            }
+            vars = cdr(vars);
+            vals = cdr(vals);
+        }
+        environment = cdr(cdr(environment));
+    }
+    fprintf(stdout, "%s is undefined\n", variable);
+    return NULL;
+}
+
+Lexeme *update(Lexeme *variable, Lexeme *value, Lexeme *environment) {
+    assert(variable != NULL);
+    assert(value != NULL);
+    assert(environment != NULL);
+    while (environment != NULL) {
+        Lexeme *vars = car(environment);
+        Lexeme *vals = cadr(environment);
+        while (vars != NULL) {
+            if (isSameVariable(variable, car(vars))) {
+                return set_car(vals, value);
+            }
+            vars = cdr(vars);
+            vals = cdr(vals);
+        }
+        environment = cdr(cdr(environment));
+    }
+    fprintf(stdout, "%s is undefined\n", variable);
+    return NULL;
+}
+
+Lexeme *insertEnvironment(Lexeme *variable, Lexeme *value, Lexeme *environment) {
+    assert(variable != NULL);
+    assert(value != NULL);
+    assert(environment != NULL);
+    set_car(environment, cons(JOIN, variable, car(env)));
+    set_car(cdr(env), cons(JOIN, value, cadr(env)));
+    return value;
+}
+
+Lexeme *extendEnvironment(Lexeme *variables, Lexeme *values, Lexeme *environment) {
+    assert(variables != NULL);
+    assert(values != NULL);
+    assert(environment != NULL);
+    return cons(ENVIRONMENT, variables, cons(ENVIRONMENT, values, environment));
 }
 
 
@@ -30,6 +88,9 @@ Lexeme *createEnvironment(void) {
 Lexeme *cons(char *type, Lexeme *left, Lexeme *right) {
     assert(type != NULL);
     Lexeme *result = newLexeme(type, NULL);
+    set_car(result, left);
+    set_cdr(result, right);
+    return result;
 }
 
 Lexeme *car(Lexeme *lexeme) {
@@ -40,4 +101,24 @@ Lexeme *car(Lexeme *lexeme) {
 Lexeme *cdr(Lexeme *lexeme) {
     assert(lexeme != NULL);
     return getLexemeRightChild(lexeme);
+}
+
+Lexeme *cadr(Lexeme *lexeme) {
+    assert(lexeme != NULL);
+    return car(cdr(lexeme));
+}
+
+void set_car(Lexeme *parent, Lexeme *child) {
+    assert(parent != NULL);
+    setLexemeLeftChild(parent, child);
+}
+
+void set_cdr(Lexeme *parent, Lexeme *child) {
+    assert(parent != NULL);
+    setLexemeRightChild(parent, child);
+}
+
+bool isSameVariable(Lexeme *var1, Lexeme *var2) {
+    assert(var1 != NULL)
+    assert(var2 != NULL)
 }
